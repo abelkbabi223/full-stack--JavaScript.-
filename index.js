c3c3cf7dc0d4c48ee2179b71dc7cfcebc73660ed
abelkbabi223 <you@example.com>
2024-11-26 06:17:51 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const book = {
+    BookTitle: 'Harry potter',
+    author: 'J. K. Rowling',
+    page: 223
+}
+
+console.log(BookTitle);
+console.log(author);
+console.log(page);
+
+
+const color = ['Yellow', 'Red', 'Green', 'Pink', 'Blue'];
+
+for (let i = 0; i < colors.length; i++){
+    console.log(color[i]);
+}
+
+let person = {
+    name: 'Abel',
+    age: 20,
+    IsStudent: true
+}
+
+person.age = 1;
+person.IsStudent = false;
+
+console.log(person);
+
+
